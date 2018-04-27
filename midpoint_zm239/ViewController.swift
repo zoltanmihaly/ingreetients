833f1125589d565fbd93c2aef42533870135ff0e
Zozó <Zozo@dhcp-ccc-9292.redrover.cornell.edu>
2018-04-27 17:52:47 -0400

    Initial Commit

---
+++ midpoint_zm239/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  midpoint_zm239
+//
+//  Created by Zozó on 2018. 04. 27..
+//  Copyright © 2018. Zozo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
